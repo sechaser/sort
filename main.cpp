615dfeedb1d71a1b055127e160bc72acc1b3cfe5
sechaser <413406236@qq.com>
2016-04-22 18:07:55 +0800

    简单选择排序

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 
+#define N 10
+
 using namespace std;
+
+void simpleSelectionSort(int a[], int n)
+{
+    int temp;
+    int index;
+
+    for(int i = 0; i < (n-1); ++ i)
+    {
+        int index = i;
+        for(int j = i + 1; j < n; ++ j)
+        {
+            if(a[j] < a[index])
+                index = j;
+        }
+
+        temp = a[index];
+        a[index] = a[i];
+        a[i] = temp;
+
+
+        for(int j = 0; j < n; ++ j)
+            std::cout<<a[j]<<" ";
+        std::cout<<std::endl;
+    }
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a[N] = {10, 8, 6, 4, 2, 1, 3, 5, 7, 9};
+
+    simpleSelectionSort(a, N);
+
     return 0;
 }
 
