0017c916d7a61a2d374fbd4003827cb9bfa704a8
sechaser <413406236@qq.com>
2016-04-25 19:56:35 +0800

    merge Sort

--- main.cpp
+++ main.cpp
@@ -1,10 +1,76 @@
 #include <iostream>
 
-using namespace std;
+#define N 10
+
+void print(int a[], int n)
+{
+    for(int i = 0; i < n; ++ i)
+        std::cout<<a[i]<<" ";
+    std::cout<<std::endl;
+}
+
+void mergeArray(int a[], int first, int mid, int last, int temp[])
+{
+    int i = first, j = mid + 1;
+    int m = mid,   n = last;
+    int k = 0;
+
+    while((i <= m) && (j <= n))
+    {
+        if(a[i] <= a[j])
+        {
+            temp[k] = a[i];
+            ++ i;
+            ++ k;
+        }
+        else
+        {
+            temp[k] = a[j];
+            ++ j;
+            ++ k;
+        }
+    }
+
+    while(i <= m)
+    {
+        temp[k] = a[i];
+        ++ i;
+        ++ k;
+    }
+
+    while(j <= n)
+    {
+        temp[k] = a[j];
+        ++ j;
+        ++ k;
+    }
+
+    for(i = 0; i < k; ++ i)
+        a[first + i] = temp[i];
+
+    print(a, N);
+}
+
+void mergeSort(int a[], int first, int last, int temp[])
+{
+    if(first < last)
+    {
+        int mid = (first + last)/2;
+        mergeSort(a, 0, mid, temp);          //左边有序
+        mergeSort(a, mid + 1, last, temp);   //右边有序
+
+        mergeArray(a, first, mid, last, temp);
+    }
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a[N] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int b[N];
+
+    mergeSort(a, 0, N-1, b);
+
     return 0;
 }
 
