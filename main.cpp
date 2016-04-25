70b5a68470a853c05c1c954c768ebd430b04e4c8
sechaser <413406236@qq.com>
2016-04-25 20:13:18 +0800

    bucket sort

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
-using namespace std;
+#define N 10
+
+void bucketSort(int a[], int b[], int n)
+{
+    for(int i = 0; i < n; ++ i)
+    {
+        b[a[i]] = a[i];
+    }
+
+    for(int i = 0; i < n; ++ i)
+        std::cout<<b[i]<<" ";
+    std::cout<<std::endl;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a[N] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int b[N];
+    bucketSort(a, b, N);
     return 0;
 }
 
