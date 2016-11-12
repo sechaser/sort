2c9c8e49eab301014b89b6cde84083f8e6391c03
sechaser <413406236@qq.com>
2016-11-12 18:24:33 +0800

    quickSort commit 2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
-#define N 10
-
-using namespace std;
+#include <vector>
+#include <iomanip>
 
 void print(int a[], int n)
 {
@@ -11,44 +9,49 @@ void print(int a[], int n)
     std::cout<<std::endl;
 }
 
-int partition(int a[], int low, int high)
+int partition(std::vector<int>& nums, int low, int high)
 {
-    int key = a[low];
+    int key = nums[low];
     int temp;
+
     while(low < high)
     {
-        while((low < high) && (a[high] >= key))
+        while(low < high && nums[high] >= key)
             -- high;
-        temp = a[low];
-        a[low] = a[high];
-        a[high] = temp;
+        temp = nums[high];
+        nums[high] = nums[low];
+        nums[low] = temp;
 
-        while((low < high) && (a[low] <= key))
+
+        while(low < high && nums[low] <= key)
             ++ low;
-        temp = a[low];
-        a[low] = a[high];
-        a[high] = temp;
+        temp = nums[high];
+        nums[high] = nums[low];
+        nums[low] = temp;
     }
-    std::cout<<"low = "<<low<<" ";
-    print(a, N);
+
+    for(std::vector<int>::size_type i = 0; i != nums.size(); ++ i)
+        std::cout<<std::setw(4)<<nums[i];
+    std::cout<<std::endl;
+
     return low;
 }
 
-void quickSort(int a[], int low, int high)
+void quickSort(std::vector<int>& nums, int low, int high)
 {
     if(low < high)
     {
-        int pos = partition(a, low, high);
-        quickSort(a, low, pos - 1);
-        quickSort(a, pos + 1, high);
+        int pos = partition(nums, low, high);
+        quickSort(nums, low, pos - 1);
+        quickSort(nums, pos + 1, high);
     }
 }
 
 int main()
 {
-    int a[N] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    std::vector<int> nums{4, 9, 1, 3, 7, 8, 2, 10, 6, 5};
 
-    quickSort(a, 0, 9);
+    quickSort(nums, 0, nums.size() - 1);
 
     return 0;
 }
